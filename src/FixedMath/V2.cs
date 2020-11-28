28067764aca0f16ca3db24ba63b039f6345690e7
Omer Faruk Sayilir <sayiliromer@gmail.com>
2020-11-28 23:17:36 +0100

    Added base v2 and v3

---
+++ src/FixedMath/V2.cs
@@ -0,0 +1,11 @@
+﻿public struct V2
+{
+    public F X;
+    public F Y;
+
+    public V2(F x, F y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ src/FixedMath/V3.cs
@@ -0,0 +1,13 @@
+﻿public struct V3
+{
+    public F X;
+    public F Y;
+    public F Z;
+
+    public V3(F x, F y, F z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
